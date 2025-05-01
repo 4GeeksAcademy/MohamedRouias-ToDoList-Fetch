2a0b2e8a4c5790fd5fe9b855a4543e021f28359d
MohamedRouias <146641169+MohamedRouias@users.noreply.github.com>
2025-05-01 21:24:11 +0000

    todolist finalizada

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+const Home = () => {
+	const [newTask, setNewTask] = useState(""); //string for the new task 
+	const [tasks, setTasks] = useState([""]); // setting an array for the list
+
+	function handleInputChange(event) {
+		setNewTask(event.target.value);
+	}
+
+	const handleKeyDown = (event) => {
+		if (event.keyCode === 13 || event.key === "Enter") {
+			// Ejecutar la acción deseada aquí
+			console.log("Enviando formulario...");
+			setTasks(t => [...t, newTask]);
+			setNewTask("");
+		}
+	};
+
+
+	function deleteTask(index) {
+		const updatedTasks = tasks.filter((_, i) => i !== index);
+		setTasks(updatedTasks);
+	}
+
+
+
+	return (
+		<>
+			<div className="text-center container">
+				<h1 className="text-center mt-5">To-Do-List</h1>
+				<input className="form-control"
+					type="text"
+					placeholder="Add new task"
+					onChange={handleInputChange} //IT SHOWS the input text that the user is typing
+					value={newTask}
+					onKeyDown={handleKeyDown}
+				/>
+				<ul>
+					{tasks.map((task, index) =>
+						<li key={index}>
+							<span className="text">{task}</span>
+							<button
+								type="button"
+								class="btn-close"
+								data-bs-dismiss="modal"
+								aria-label="Close"
+								onClick={() => deleteTask(index)}>
+							</button>
+						</li>
+					)}
+				</ul>
+				<div className="task-counter">
+					{tasks.length} {tasks.length === 1 ? "item" : "items"} left
+				</div>
+			</div>
+
+		</>
 	);
 };
 
